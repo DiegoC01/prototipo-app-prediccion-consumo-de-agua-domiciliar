413e37fafe64ed9f004cb09f520f0e2e74f2f43d
DiegoC01 <diego_canquil@outlook.es>
2023-12-17 07:59:50 -0300

    Guardado de archivos faltantes

--- android/app/src/main/kotlin/com/example/flutter_application_1/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_application_1/MainActivity.kt
@@ -1,6 +1,3 @@
-package com.example.flutter_application_1
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:762e507c9c68174079dd49b8d3611f686a15efbc2d1dbc7d2d9154fef1eb7da3
+size 144
